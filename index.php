97cf8c019515489d10cee41bd447168a25bacc9a
sandriamaulana <48830382+sandriamaulana@users.noreply.github.com>
2019-03-22 23:20:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Registration_new (name, email, job, date) 
+            $sql_insert = "INSERT INTO Registration_remake (name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -60,7 +60,7 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registration_new";
+            $sql_select = "SELECT * FROM Registration_remake";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
