34e337b382dc157fcbe20ecafa3106ecb24aa62e
sandriamaulana <48830382+sandriamaulana@users.noreply.github.com>
2019-03-22 22:51:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Registration_new ('',name, email, job, date) 
+            $sql_insert = "INSERT INTO Registration_new (name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
